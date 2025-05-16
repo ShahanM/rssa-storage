6ae517b16753d89532b9b3dd904c6217407ed166
Mehtab "Shahan" Iqbal <mehtabi@clemson.edu>
2025-05-16 00:33:59 -0400

    added timezone to access log

---
+++ rssadb/migrations/versions/2025_05_16_0029-9eb9100a5c5a_alter_timestamp_column_in_access_log.py
@@ -0,0 +1,40 @@
+"""alter timestamp column in access_log
+
+Revision ID: 9eb9100a5c5a
+Revises: 3cc7a35b9567
+Create Date: 2025-05-16 00:29:40.497668
+
+"""
+
+from typing import Sequence, Union
+from datetime import datetime, timezone
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "9eb9100a5c5a"
+down_revision: Union[str, None] = "3cc7a35b9567"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.alter_column(
+        "access_log",
+        "timestamp",
+        type_=sa.TIMESTAMP(timezone=True),
+        existing_type=sa.DateTime,
+    )
+
+
+def downgrade() -> None:
+    op.alter_column(
+        "access_log",
+        "timestamp",
+        type_=sa.DateTime,
+        existing_type=sa.TIMESTAMP(timezone=True),
+    )
