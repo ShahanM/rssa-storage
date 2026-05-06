7ade9c133519dfa0aabd4ce07681836cb784c71e
Mehtab "Shahan" Iqbal <mehtabi@clemson.edu>
2026-05-06 18:10:36 -0400

    explicitly added models to the __init__ file for rssadb

--- src/rssa_storage/rssadb/models/__init__.py
+++ src/rssa_storage/rssadb/models/__init__.py
@@ -1,3 +1,82 @@
-from .rssa_base_models import RssaBase as Base
+"""
+Treating this as a model registry aggregator.
+"""
 
-__all__ = ['Base']
+from .participant_movie_sequence import (
+    PreShuffledMovieList,
+    ShuffledMovieListItem,
+    StudyParticipantMovieSession,
+)
+from .participant_responses import (
+    Feedback,
+    ParticipantAttentionCheckResponse,
+    ParticipantFreeformResponse,
+    ParticipantInteractionLog,
+    ParticipantRating,
+    ParticipantStudyInteractionResponse,
+    ParticipantSurveyResponse,
+)
+from .rssa_base_models import RssaBase as Base
+from .study_components import (
+    ApiKey,
+    ElicitationPolicy,
+    Study,
+    StudyAttentionCheck,
+    StudyAuthorization,
+    StudyCondition,
+    StudyStep,
+    StudyStepPage,
+    StudyStepPageContent,
+    User,
+)
+from .study_participants import (
+    Demographic,
+    ParticipantRecommendationContext,
+    ParticipantStudySession,
+    StudyParticipant,
+    StudyParticipantType,
+)
+from .survey_constructs import (
+    SurveyConstruct,
+    SurveyItem,
+    SurveyScale,
+    SurveyScaleLevel,
+)
+
+__all__ = [
+    'Base',
+    # Study Components
+    'ApiKey',
+    'ElicitationPolicy',
+    'Study',
+    'StudyAttentionCheck',
+    'StudyAuthorization',
+    'StudyCondition',
+    'StudyStep',
+    'StudyStepPage',
+    'StudyStepPageContent',
+    'User',
+    # Survey Constructs
+    'SurveyConstruct',
+    'SurveyItem',
+    'SurveyScale',
+    'SurveyScaleLevel',
+    # Study Participants
+    'Demographic',
+    'ParticipantRecommendationContext',
+    'ParticipantStudySession',
+    'StudyParticipant',
+    'StudyParticipantType',
+    # Participant Responses
+    'Feedback',
+    'ParticipantAttentionCheckResponse',
+    'ParticipantFreeformResponse',
+    'ParticipantInteractionLog',
+    'ParticipantRating',
+    'ParticipantStudyInteractionResponse',
+    'ParticipantSurveyResponse',
+    # Participant Movie Sequence
+    'PreShuffledMovieList',
+    'ShuffledMovieListItem',
+    'StudyParticipantMovieSession',
+]
